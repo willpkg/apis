d8f19f1b42d6e541ce2bc66bbe26ed5157faf2ce
uimaxbai <uimax.bai@gmail.com>
2024-03-13 14:12:38 +0000

    f

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,37 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import packages from './packages.json';
 
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
-}
+export interface Env {}
+export interface ExecutionContext {}
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		const url = new URL(request.url);
+		var packages = JSON
+		if (url.pathname === "/latest") {
+			return new Response('0.1.0');
+		}
+		else if (url.pathname === "/package") {
+			const urlParams = new URLSearchParams(window.location.search);
+			if (urlParams.get("p") === null) {
+				return new Response("Missing quqery parameter p.", {
+					status: 400,
+					statusText: "Missing quqery parameter p."
+				})
+			}
+			else if (urlParams.get("p") in packages && urlParams.get("p") !== null) {
+
+			}
+		}
+		else {
+			return new Response("404 Not Found", {
+				status: 404,
+				statusText: "The requested resource was not found."
+			});
+		}
+		// Fallback
+		return new Response("500 Internal Server Error", {
+			status: 500,
+			statusText: "An unknown error occured on the server."
+		});
 	},
 };
