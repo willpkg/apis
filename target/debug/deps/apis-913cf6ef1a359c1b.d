9464d92719389fe0acf1d8242b83b8950e4bd6cf
werdl <116349156+werdl@users.noreply.github.com>
2024-04-08 19:04:38 +0100

    revert

---
+++ target/debug/deps/apis-913cf6ef1a359c1b.d
@@ -0,0 +1,6 @@
+C:\Users\rober\coding\apis\target\debug\deps\libapis-913cf6ef1a359c1b.rmeta: src\main.rs src\triple.rs
+
+C:\Users\rober\coding\apis\target\debug\deps\apis-913cf6ef1a359c1b.d: src\main.rs src\triple.rs
+
+src\main.rs:
+src\triple.rs:
